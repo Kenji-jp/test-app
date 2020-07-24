73ef935fbcf02d1919b236e9aa8be2680322599f
kenji <tora169mm@gmail.com>
2020-07-24 10:43:11 +0900

    [fix]validate for article

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,4 @@
 class Article < ApplicationRecord
-    validates: title,       presence:true, length: { minimum:  6, maxmum:100 }
-    validates: description, presence:true, length: { minimum: 10, maxmum:300 }
+    validates:title,       presence:true, length: { minimum:  6, maxmum:100 }
+    validates:description, presence:true, length: { minimum: 10, maxmum:300 }
 end
