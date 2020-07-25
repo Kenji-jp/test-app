b635c8215156dacf9279a3cf974413e77c7df135
kenji <tora169mm@gmail.com>
2020-07-25 15:49:03 +0900

    [add] one-to-many association between users and articles from backend

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,5 @@
 class Article < ApplicationRecord
-    validates:title,       presence:true, length: { minimum:  6, maxmum:100 }
-    validates:description, presence:true, length: { minimum: 10, maxmum:300 }
+    belongs_to :user
+    validates :title,       presence:true, length: { minimum:  6, maxmum:100 }
+    validates :description, presence:true, length: { minimum: 10, maxmum:300 }
 end
