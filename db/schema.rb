62f00ed5ca2d46a2bd81ee6289a4c7e670436c15
kenji <tora169mm@gmail.com>
2020-07-25 13:58:02 +0900

    [add] user validations

--- db/schema.rb
+++ db/schema.rb
@@ -20,6 +20,10 @@ ActiveRecord::Schema.define(version: 20200725044116) do
   end
 
   create_table "users", force: :cascade do |t|
+    t.string "username"
+    t.string "email"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
 end
