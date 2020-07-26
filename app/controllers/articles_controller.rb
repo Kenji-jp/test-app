3753ae1a1b67d9f35231e16945e1d209f94714af
kenji <tora169mm@gmail.com>
2020-07-26 13:02:20 +0900

    paginate to view

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,7 +3,7 @@ class ArticlesController < ApplicationController
   before_action :set_article, only: [:show, :edit, :update, :destroy]
 
   def index
-    @articles = Article.paginate(page: params[:page], per_page: 5)
+    @articles = Article.all
   end
 
   def show
